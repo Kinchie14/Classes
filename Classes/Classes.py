35899abfaa04319aa9d53cbd9eaeced770ade989
Vincent Vargas <vincentvargas6969@gmail.com>
2022-08-06 02:26:43 -0700

    Fixing the Error

--- Classes/Classes.py
+++ Classes/Classes.py
@@ -1,21 +1,21 @@
 class Motors:
 
     def __init__ (self, make, model, size_of_wheels, color, engine_type, engine_size):
-        make = None
-        model = None
-        size_of_wheels = None
-        color = None
-        engine_type = None
-        engine_size = None
+        self.make = make
+        self.model = model
+        self.size_of_wheels = size_of_wheels
+        self.color = color
+        self.engine_type = engine_type
+        self.engine_size = engine_size
     
 
 class Car:
 
     def __init__ (self, doors, color, make, model, engine_type, engine_size):
-        doors = None
-        color = None
-        make = None
-        model = None
-        engine_type = None
-        engine_size = None
+        self.doors = doors
+        self.color = color
+        self.make = make
+        self.model = model
+        self.engine_type = engine_type
+        self.engine_size = engine_size
     
