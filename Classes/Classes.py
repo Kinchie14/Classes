fc8da96598a0f29ef63615f500c36b76b7e43ac3
Vincent Vargas <vincentvargas6969@gmail.com>
2022-08-06 02:17:51 -0700

    Initial Commit

---
+++ Classes/Classes.py
@@ -0,0 +1,21 @@
+class Motors:
+
+    def __init__ (self, make, model, size_of_wheels, color, engine_type, engine_size):
+        make = None
+        model = None
+        size_of_wheels = None
+        color = None
+        engine_type = None
+        engine_size = None
+    
+
+class Car:
+
+    def __init__ (self, doors, color, make, model, engine_type, engine_size):
+        doors = None
+        color = None
+        make = None
+        model = None
+        engine_type = None
+        engine_size = None
+    
